8c4c2c9ea14e26b616260a5dc387bd0c34537209
asisaaa <105841105222@student.unismuh.ac.id>
2024-08-14 13:57:51 +0800

    selesai chapter 5

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
+import { lusitana } from "./ui/fonts";
 import '@/app/ui/global.css'
+// /app/layout.jsx
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,25 +1,27 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image';
+
 
 export default function Page() {
   return (
-    <main className="flex flex-col min-h-screen p-6">
-      <div className="flex items-end h-20 p-4 bg-red-500 rounded-lg shrink-0 md:h-52">
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-green-500 p-4 md:h-52">
         <AcmeLogo /> 
       </div>
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
           <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-red-500">
+            <a href="https://nextjs.org/learn/" className="text-green-500">
               Next.js Learn Course
             </a>
             , brought to you by devnolife - laboratorium-if-unismuh.
           </p>
           <Link
             href="/login"
-            className="flex items-center self-start gap-5 px-6 py-3 text-sm font-medium text-white transition-colors bg-blue-500 rounded-lg hover:bg-blue-400 md:text-base"
+            className="flex items-center self-start gap-5 px-6 py-3 text-sm font-medium text-white transition-colors bg-green-500 rounded-lg hover:bg-green-400 md:text-base"
           >
             <span>Log in</span>
           </Link>
@@ -28,8 +30,18 @@ export default function Page() {
           {/* Add Hero Images Here */}
           <img
   src="/hero-desktop.png"
-  alt="Screenshots of the dashboard project showing desktop version"
-  />
+        width={1000}
+        height={760}
+        className="hidden md:block"
+   alt="Screenshots of the dashboard project showing desktop version"
+/>
+<img
+  src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+   alt="Screenshots of the dashboard project showing desktop version"
+/>
         </div>
         <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
       </div>
